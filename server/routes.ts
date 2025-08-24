5b295bfc22a8de33749e1719a316cc2537a6f136
duderayuh <129043037+duderayuh@users.noreply.github.com>
2025-08-24 23:44:12 +0000

    significant changes to audio playback, transcription quality, apple maps instead of google, and an audio playbar.

--- server/routes.ts
+++ server/routes.ts
@@ -2278,7 +2278,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
             'Content-Length': result.audio.length.toString()
           });
           res.end(result.audio);
-}
+        }
         
       } catch (dbError) {
         if (db) db.close();
